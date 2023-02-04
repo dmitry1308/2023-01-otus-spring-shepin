dd6b5a4a0c315938afde46e634c78f79a569ded4
Schepin Dmitriy <bright1308@gmail.com>
2023-02-04 11:38:53 +0700

    task-2: change context to annotation

--- tasks/task-2/src/main/java/ru/otus/shepin/spring/Main.java
+++ tasks/task-2/src/main/java/ru/otus/shepin/spring/Main.java
@@ -1,11 +1,12 @@
 package ru.otus.shepin.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.shepin.spring.controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("spring-context.xml");
 
         Controller controller = context.getBean(Controller.class);
         controller.handle();
