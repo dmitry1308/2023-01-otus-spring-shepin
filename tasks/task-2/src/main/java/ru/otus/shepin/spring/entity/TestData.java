97b7a7b0179191cd6a337943cd45c0fdf35ecd28
Schepin Dmitriy <bright1308@gmail.com>
2023-02-09 23:16:28 +0300

    task-2: correct import

--- tasks/task-2/src/main/java/ru/otus/shepin/spring/entity/TestData.java
+++ tasks/task-2/src/main/java/ru/otus/shepin/spring/entity/TestData.java
@@ -1,9 +1,7 @@
 package ru.otus.shepin.spring.entity;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 
 @Builder(toBuilder = true)
 @Getter
