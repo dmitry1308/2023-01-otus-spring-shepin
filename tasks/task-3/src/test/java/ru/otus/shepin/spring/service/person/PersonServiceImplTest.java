43230ab767b3ecfcc90985920267f374639a7f17
Schepin Dmitriy <bright1308@gmail.com>
2023-02-23 16:39:57 +0300

    task-4: change test to SpringBootTest

--- tasks/task-3/src/test/java/ru/otus/shepin/spring/service/person/PersonServiceImplTest.java
+++ tasks/task-3/src/test/java/ru/otus/shepin/spring/service/person/PersonServiceImplTest.java
@@ -6,13 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
-import ru.otus.shepin.spring.config.AppTestProps;
 import ru.otus.shepin.spring.entity.Person;
 import ru.otus.shepin.spring.service.io.InputService;
 import ru.otus.shepin.spring.service.io.OutputService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,35 +18,25 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @DisplayName("Test enter person data")
 class PersonServiceImplTest {
-
     @Autowired
     private PersonServiceImpl personDataService;
-    @Autowired
-    private AppTestProps      appTestProps;
     @MockBean
     private InputService      inputService;
     @MockBean
     private OutputService     outputService;
-    @MockBean
-    private MessageSource     messageSource;
 
     @BeforeEach
     void setUp() {
         when(inputService.readLine()).thenReturn("Dmitry", String.valueOf(36));
-        when(messageSource.getMessage("question.name.user", null, appTestProps.getLocale())).thenReturn("text");
     }
 
     @Test
     void when_call_method_askPersonAndGetAnswer_return_ready_object() {
-        when(inputService.readLine()).thenReturn("Dmitry", String.valueOf(36));
-
-        when(messageSource.getMessage("question.name.user", null, appTestProps.getLocale())).thenReturn("text");
-
         verify(outputService,times(2));
-
         Person personData = personDataService.getPersonData();
-        assertEquals("Dmitry", personData.getName());
-        assertEquals(36, personData.getAge());
+
+        assertThat(personData.getName()).isEqualTo("Dmitry");
+        assertThat(personData.getAge()).isEqualTo(36);
     }
 }
 
