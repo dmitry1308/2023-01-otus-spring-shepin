7174a5a09e3a01eb9aa609f17b18c65976916b0b
Schepin Dmitriy <bright1308@gmail.com>
2023-02-25 21:21:55 +0300

    master: create library project

---
+++ library-tasks/task-1/src/test/java/ru/otus/spring/shepin/Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.shepin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
