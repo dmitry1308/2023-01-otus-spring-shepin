df68a3781d0e40712608f4922f7db802c5125c75
Schepin Dmitriy <bright1308@gmail.com>
2023-02-26 00:37:46 +0300

    library-task-1: add code

--- 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/Main.java
+++ 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/Main.java
@@ -3,11 +3,14 @@ package ru.otus.spring.shepin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// url h2 консоли: http://localhost:8081/h2-console
+// url базы: jdbc:h2:mem:librarydb
 @SpringBootApplication
-public class Application {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
