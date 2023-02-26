e7b859b4fe3ba60c435f4053fadf2424bf3bc049
Schepin Dmitriy <bright1308@gmail.com>
2023-02-26 18:41:28 +0300

    library-task-1: add yaml with crate entity

--- 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/Main.java
+++ 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/Main.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // url базы: jdbc:h2:mem:librarydb
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
