3a72cba12658cfc768a93a5e345415372cc5d5cc
Schepin Dmitriy <bright1308@gmail.com>
2023-03-02 23:12:29 +0300

    library-task-1: add tests

---
+++ 2-library-tasks/task-1/src/test/java/ru/otus/spring/shepin/dao/BookDaoJdbcTest.java
@@ -0,0 +1,32 @@
+package ru.otus.spring.shepin.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDaoJdbcTest {
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
---
+++ 2-library-tasks/task-1/src/test/java/ru/otus/spring/shepin/service/BookServiceJdbcTest.java
@@ -0,0 +1,32 @@
+package ru.otus.spring.shepin.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceJdbcTest {
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateByName() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
