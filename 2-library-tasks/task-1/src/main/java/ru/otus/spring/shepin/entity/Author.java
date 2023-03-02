899fef60c09f98383f809eaf2e07fd0e1897a7d0
Schepin Dmitriy <bright1308@gmail.com>
2023-03-02 23:04:56 +0300

    library-task-1: add methods

--- 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/entity/Author.java
+++ 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/entity/Author.java
@@ -4,10 +4,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 public class Author {
     private Long   id;
     private String lastName;
     private String firstName;
+
+    private List<Book> bookList;
 }
--- 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/entity/Genre.java
+++ 2-library-tasks/task-1/src/main/java/ru/otus/spring/shepin/entity/Genre.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 public class Genre {
     private Long   id;
     private String name;
+    private List<Book> bookList;
 
 }
