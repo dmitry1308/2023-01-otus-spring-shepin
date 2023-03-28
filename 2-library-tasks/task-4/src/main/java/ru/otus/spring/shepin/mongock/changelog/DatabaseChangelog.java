4e50b77e69d24327ecf14288c53cdb47a52a7ea6
d.schepin <d.shcepin@comitas.ru>
2023-03-28 20:25:03 +0300

    library-task-4: add changesets

--- 2-library-tasks/task-4/src/main/java/ru/otus/spring/shepin/mongock/changelog/DatabaseChangelog.java
+++ 2-library-tasks/task-4/src/main/java/ru/otus/spring/shepin/mongock/changelog/DatabaseChangelog.java
@@ -5,7 +5,11 @@ import com.github.cloudyrock.mongock.ChangeSet;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import ru.otus.spring.shepin.dao.AuthorRepository;
+import ru.otus.spring.shepin.dao.BookRepository;
 import ru.otus.spring.shepin.dao.GenreRepository;
+import ru.otus.spring.shepin.entity.Author;
+import ru.otus.spring.shepin.entity.Book;
 import ru.otus.spring.shepin.entity.Genre;
 
 @ChangeLog
@@ -28,5 +32,35 @@ public class DatabaseChangelog {
     public void insertGenre2(GenreRepository repository) {
         Genre genre = Genre.builder().name("Genre-2").build();
         repository.save(genre);
+    }
+
+
+    @ChangeSet(order = "004", id = "insertAuthor1", author = "dshepin")
+    public void insertAuthor2(MongoDatabase db) {
+        MongoCollection<Document> myCollection = db.getCollection("author");
+
+        var doc = new Document().append("firstName", "first name 1").append("lastName", "last name 1");
+        myCollection.insertOne(doc);
+    }
+
+
+    @ChangeSet(order = "005", id = "insertAuthor2", author = "dshepin")
+    public void insertAuthor2(AuthorRepository repository) {
+        Author author = Author.builder().firstName("first name 2").lastName("last name 2").build();
+        repository.save(author);
+    }
+
+    @ChangeSet(order = "006", id = "insertBook1", author = "dshepin")
+    public void insertBook1(MongoDatabase db) {
+        MongoCollection<Document> myCollection = db.getCollection("book");
+
+        Document document = new Document().append("name", "Book1");
+        myCollection.insertOne(document);
+    }
+
+    @ChangeSet(order = "007", id = "insertBook2", author = "dshepin")
+    public void insertBook2(BookRepository repository) {
+        Book book = Book.builder().name("Book2").build();
+        repository.save(book);
     }
 }
