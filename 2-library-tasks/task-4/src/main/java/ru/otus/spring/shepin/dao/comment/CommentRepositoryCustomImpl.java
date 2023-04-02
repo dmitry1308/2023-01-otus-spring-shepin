4a3a6b071e465696084775e9f057e476a18fa6c9
d.schepin <d.shcepin@comitas.ru>
2023-04-02 23:23:08 +0300

    library-task-4:delete old code

--- 2-library-tasks/task-4/src/main/java/ru/otus/spring/shepin/dao/comment/CommentRepositoryCustomImpl.java
+++ 2-library-tasks/task-4/src/main/java/ru/otus/spring/shepin/dao/comment/CommentRepositoryCustomImpl.java
@@ -8,8 +8,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import ru.otus.spring.shepin.entity.Comment;
 
-import java.util.List;
-
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @RequiredArgsConstructor
@@ -21,13 +19,5 @@ public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
         val       query  = Query.query(where("$id").is(new ObjectId(id)));
         val update = new Update().pull("comment", query);
         mongoTemplate.updateMulti(new Query(), update, Comment.class);
-    }
-
-
-    public List<Comment> findByBookId(String bookId) {
-        Query query = new Query();
-        query.addCriteria(where("id").in(new ObjectId("64249d675adddf3fd6ddc767")));
-         mongoTemplate.find(query, Comment.class);
-        return null;
     }
 }
