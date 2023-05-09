3cbaf6f4f08b8eb3bbd7d586249928e178800186
d.schepin <d.shcepin@comitas.ru>
2023-05-09 11:38:16 +0300

    library-task-6_ajax: change code

--- 2-library-tasks/task-6_ajax/src/main/java/ru/otus/spring/shepin/controller/BookPagesController.java
+++ 2-library-tasks/task-6_ajax/src/main/java/ru/otus/spring/shepin/controller/BookPagesController.java
@@ -1,4 +1,4 @@
-package ru.otus.spring.shepin.page;
+package ru.otus.spring.shepin.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
--- 2-library-tasks/task-6_ajax/src/test/java/ru/otus/spring/shepin/controller/BookControllerTest.java
+++ 2-library-tasks/task-6_ajax/src/test/java/ru/otus/spring/shepin/controller/BookControllerTest.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.otus.spring.shepin.page.BookPagesController;
 import ru.otus.spring.shepin.service.AuthorService;
 import ru.otus.spring.shepin.service.GenreService;
 
