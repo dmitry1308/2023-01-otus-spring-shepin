570da8be2c46dc220f0ab0ff4aaf59620cf01da2
d.schepin <d.shcepin@comitas.ru>
2023-05-22 23:19:14 +0300

    task-7_webflux_mongo: change code

--- 2-library-tasks/task-7_webflux_mongo/src/main/java/ru/otus/spring/shepin/dao/AuthorRepository.java
+++ 2-library-tasks/task-7_webflux_mongo/src/main/java/ru/otus/spring/shepin/dao/AuthorRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 import ru.otus.spring.shepin.entity.Author;
 
-import java.util.Optional;
-
 public interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
     Mono<Author> findByFirstNameAndLastName(String firstName, String lastName);
 }
